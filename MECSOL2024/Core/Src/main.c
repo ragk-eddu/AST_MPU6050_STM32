0d2687156f2beb352acf271389a7a8a0af269fd3
Eduardo Cardoso <88335879+ragk-eddu@users.noreply.github.com>
2024-10-09 14:04:43 -0300

    1.2.Accel4NotWorking

--- MECSOL2024/Core/Src/main.c
+++ MECSOL2024/Core/Src/main.c
@@ -51,6 +51,7 @@ MPU6050_t MPU60502;
 MPU6050_t MPU60503;
 //MPU6050_t MPU60504;
 MPU6050_t MPU60505;
+double time;
 //MPU6050_t MPU60506;
 /* USER CODE END PV */
 
@@ -99,9 +100,9 @@ int main(void)
   MX_I2C2_Init();
   MX_I2C3_Init();
   /* USER CODE BEGIN 2 */
-//  HAL_I2C_Init(&hi2c1);
-//  while (MPU6050_Init(&MPU60501, &hi2c1, MPU6050_ADDR) == 1);
-//  while (MPU6050_Init(&MPU60502, &hi2c1, MPU6050_ADDR+2) == 1);
+  HAL_I2C_Init(&hi2c1);
+  while (MPU6050_Init(&MPU60501, &hi2c1, MPU6050_ADDR) == 1);
+  while (MPU6050_Init(&MPU60502, &hi2c1, MPU6050_ADDR+2) == 1);
   HAL_I2C_Init(&hi2c2);
   while (MPU6050_Init(&MPU60503, &hi2c2, MPU6050_ADDR) == 1);
 //  while (MPU6050_Init(&MPU60504, &hi2c2, MPU6050_ADDR+2) == 1);
@@ -112,25 +113,30 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  time = 0;
+   while (1)
+   {
+     /* USER CODE END WHILE */
+
+ 	  time = (double) (HAL_GetTick()) / 1000;
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
 
-//	  MPU6050_Read_All(&hi2c1, &MPU60501, 1, 1, 1);
-//	  MPU6050_Read_All(&hi2c1, &MPU60502, 1, 1, 1);
+	  MPU6050_Read_All(&hi2c1, &MPU60501, 1, 1, 1);
+	  MPU6050_Read_All(&hi2c1, &MPU60502, 1, 1, 1);
 	  MPU6050_Read_All(&hi2c2, &MPU60503, 1, 1, 1);
 //	  MPU6050_Read_All(&hi2c2, &MPU60504, 1, 1, 1);
 	  MPU6050_Read_All(&hi2c3, &MPU60505, 1, 1, 1);
 //	  MPU6050_Read_All(&hi2c3, &MPU60506, 1, 1, 1);
 
-//	  printf("%f\t",MPU60501.KalmanAngleX);
-//	  printf("%f\t",MPU60502.KalmanAngleX);
+	  printf("%f\t",MPU60501.KalmanAngleX);
+	  printf("%f\t",MPU60502.KalmanAngleX);
 	  printf("%f\t",MPU60503.KalmanAngleX);
 //	  printf("%f\t",MPU60504.KalmanAngleX);
 	  printf("%f\t",MPU60505.KalmanAngleX);
 //	  printf("%f\t\n\r",MPU60506.KalmanAngleX);
+	  printf("%f\t\n\r", time);
 	  HAL_Delay (10);
   }
   /* USER CODE END 3 */
